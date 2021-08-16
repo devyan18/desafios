4de66916a851d3f3755a452f948d0d6399e097e8
devyan18 <epache17@gmail.com>
2021-08-16 18:41:13 -0300

    change of syntax for rellenarse.js

--- rellenarse.js
+++ rellenarse.js
@@ -1,35 +1,19 @@
-const codi = (works) => {
+const codi = (works = "") => {
   const codification = (works) => {
     let work = works.toString()
-    const noWork = work.length
-    if(  noWork % 5 == 0 ){
-      return work 
-    } 
-    else{
-      const len = work.length % 5
-      const arr = (5 - len)
-      for(let i = 0; i < arr ; i++){
-        work += arr
-      } 
-      return work
-    } 
+    if(work.length % 5 == 0) return work 
+    const len = work.length % 5
+    const arr = (5 - len)
+    for(let i = 0; i < arr ; i++){work += arr} 
+    return work.toString()
   }
-  const param = codification(works)
-  console.log("Codificado: ", param)
-
-
   const decodification = (works) => {
     const deleting = parseInt(works[works.length-1])
-    if(!deleting){
-      return(works)
-    }else{
-      const find = works.length - deleting
-      return(works.slice(0, find))
-    }
-  }
-
-  const paramD = decodification(works)
-  console.log("Decodificado: ", paramD)
+    if(!deleting)return works.toString()
+    const find = works.length - deleting
+    return(works.slice(0, find).toString())
+  } 
+  console.log("Codificado: ", codification(works))
+  console.log("Decodificado: ", decodification(works))
 }
-
-codi("")
+codi("holanda")
