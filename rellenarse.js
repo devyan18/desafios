f4d0a7ad9158532b6810856598db526df2a37f71
devyan18 <epache17@gmail.com>
2021-08-16 23:06:27 -0300

    eslit added

--- rellenarse.js
+++ rellenarse.js
@@ -1,19 +1,19 @@
-const codi = (works = "") => {
+const codi = (works = '') => {
   const codification = (works) => {
     let work = works.toString()
-    if(work.length % 5 == 0) return work 
+    if (work.length % 5 === 0) return work
     const len = work.length % 5
     const arr = (5 - len)
-    for(let i = 0; i < arr ; i++){work += arr} 
+    for (let i = 0; i < arr; i++) { work += arr }
     return work.toString()
   }
   const decodification = (works) => {
-    const deleting = parseInt(works[works.length-1])
-    if(!deleting)return works.toString()
+    const deleting = parseInt(works[works.length - 1])
+    if (!deleting) return works.toString()
     const find = works.length - deleting
-    return(works.slice(0, find).toString())
-  } 
-  console.log("Codificado: ", codification(works))
-  console.log("Decodificado: ", decodification(works))
+    return (works.slice(0, find).toString())
+  }
+  console.log('Codificado: ', codification(works))
+  console.log('Decodificado: ', decodification(works))
 }
-codi("holanda")
+codi('work')
